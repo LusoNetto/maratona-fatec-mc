4f3d781941ead62093c30a4a4ccb5f6d8154c0f5
Luso Netto <luciano.silva137@hotmail.com>
2023-03-19 18:20:46 -0300

    solve question 1078 beecrowd

---
+++ codes-beecrowd/1078.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n = 0;
+    cin >> n;
+    // printando o resultado de cada conta do 1 até o 10
+    for(int i = 1; i <= 10; i++){
+        cout << i << " x " << n << " = " << i*n << endl;
+    }
+}
