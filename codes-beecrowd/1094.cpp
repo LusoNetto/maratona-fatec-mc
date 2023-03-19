d6f8aa7d11788100b26c4ffc747898e130531fa7
Luso Netto <luciano.silva137@hotmail.com>
2023-03-19 18:52:32 -0300

    solve question 1094 beecrowd

---
+++ codes-beecrowd/1094.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n = 0;
+    int cobaias = 0, coelhos = 0, ratos = 0, sapos = 0;
+    double quantidade = 0;
+    string tipo = "";
+    cin >> n;
+    cout << fixed;
+    cout << setprecision(2);
+    for (int i = 0; i< n; i++){
+         cin >> quantidade >> tipo;
+         if(tipo == "C"){
+             coelhos += quantidade;
+             cobaias += quantidade;
+             quantidade = 0;
+         } else if(tipo == "R") {
+             ratos += quantidade;
+             cobaias += quantidade;
+             quantidade = 0;
+         } else if(tipo == "S") {
+             sapos += quantidade;
+             cobaias += quantidade;
+             quantidade = 0;
+         }
+    }
+    double c = 0, r = 0, s = 0;
+    c = coelhos;
+    r = ratos;
+    s = sapos;
+    cout << "Total: " <<  cobaias  << " cobaias" << endl;
+    cout << "Total de coelhos: " << coelhos << endl;
+    cout << "Total de ratos: " << ratos << endl;
+    cout << "Total de sapos: " << sapos << endl;
+    cout << "Percentual de coelhos: " << c/cobaias*100 << " %" << endl;
+    cout << "Percentual de ratos: " << r/cobaias*100 << " %" << endl;
+    cout << "Percentual de sapos: " << s/cobaias*100 << " %" << endl;
+}
