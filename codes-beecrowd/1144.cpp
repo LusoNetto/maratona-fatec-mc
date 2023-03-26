5fd8788e48f0b30175631c11a43dcdacb4103795
Luso Netto <luciano.silva137@hotmail.com>
2023-03-26 13:31:00 -0300

    remove contador

--- codes-beecrowd/1144.cpp
+++ codes-beecrowd/1144.cpp
@@ -4,7 +4,6 @@ int main() {
     ios_base :: sync_with_stdio(0); cin.tie(0);
     int n = 0;
     cin >> n;
-    int contador = 0;
     for(int i = 1; i<=n; i++){
         cout << i << " " << i*i << " " << i*i*i << "\n";
         cout << i << " " << i*i+1 << " " << i*i*i+1 << "\n";
