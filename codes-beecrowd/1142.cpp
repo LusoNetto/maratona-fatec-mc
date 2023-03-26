816e5bb119ff833998bede134bccb388fd0fbcb5
Luso Netto <luciano.silva137@hotmail.com>
2023-03-26 12:58:45 -0300

    solve 1142 beecrowd

---
+++ codes-beecrowd/1142.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    int n = 0;
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        cout << 4*i-3 << " " << 4*i-2 << " " << 4*i-1 << " PUM\n";
+    }
+}
