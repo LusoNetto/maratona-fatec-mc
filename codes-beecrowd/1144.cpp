3fc9546d748d518cd34de03686ff2a6e228e241b
Luso Netto <luciano.silva137@hotmail.com>
2023-03-26 13:28:08 -0300

    solve 1144 beecrowd

---
+++ codes-beecrowd/1144.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base :: sync_with_stdio(0); cin.tie(0);
+    int n = 0;
+    cin >> n;
+    int contador = 0;
+    for(int i = 1; i<=n; i++){
+        cout << i << i*i << i*i*i << "\n";
+        cout << i << i*i+1 << i*i*i+1 << "\n";
+    }
+}
